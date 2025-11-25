a339288f40447f62809af837306304642aef6f0d
julianamschwand <j.amschwand@hotmail.com>
2025-11-25 13:24:44 +0100

    Made server file

---
+++ Backend/server.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import session from 'express-session'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+import { createSessionStore } from './database/db.js'
+
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+app.use(cors({
+  origin: process.env.ORIGIN_URL,
+  credentials: true
+}))
+app.set('trust proxy', Number(process.env.PROXY_NUMBER))
+
+const sessionStore = createSessionStore()
+
+const isProd = process.env.NODE_ENV == "production"
+
+// express session config
+app.use(session({
+  key: "SessionId",
+  secret: process.env.SESSION_SECRET,
+  store: sessionStore,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: isProd,
+    httpOnly: true,
+    sameSite: isProd ? "strict" : "lax",
+    maxAge: 1000 * 60 * 60 * 24 * 7
+  }
+}))
+
+const port = process.env.PORT
+app.listen(port, () => console.log(`Server running on port ${port}`))
