8a93ac6ee1d745b37cb4ca79180358f6a8263f94
Janik Stucki <Janik_Stucki@sluz.ch>
2025-12-02 14:50:02 +0100

    commit

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -2,6 +2,7 @@
 import { RouterView } from 'vue-router';
 import router from './router';
 import NavBar from './components/NavBar.vue';
+import HomeView from './views/HomeView.vue';
 import SideBar from './components/SideBar.vue';
 
 </script>
