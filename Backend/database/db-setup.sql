4e8fa20e00070c6e64a7cc7c076fb089dd31b4da
Benjamin Maier <benjamin_maier@sluz.ch>
2026-01-11 19:56:33 +0100

    change db-setup.sql to non capital letters

--- Backend/database/db-setup.sql
+++ Backend/database/db-setup.sql
@@ -1,65 +1,65 @@
-CREATE TABLE Users(
+CREATE TABLE users(
   userId INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(50) UNIQUE,
   email VARCHAR(254) UNIQUE,
   `password` VARCHAR(256)
 );
 
-CREATE TABLE Organizations(
+CREATE TABLE organizations(
   organizationId INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   `name` VARCHAR(50) UNIQUE,
   `description` TEXT
 );
 
-CREATE TABLE OrganizationUsers(
+CREATE TABLE organization_users(
   organizationUserId INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   userRole enum('user','admin','owner'),
   fk_UserId INT UNSIGNED,
   fk_OrganizationId INT UNSIGNED,
   UNIQUE (fk_UserId, fk_OrganizationId),
-  FOREIGN KEY (fk_UserId) REFERENCES Users(userId) ON DELETE CASCADE,
-  FOREIGN KEY (fk_OrganizationId) REFERENCES Organizations(organizationId) ON DELETE CASCADE
+  FOREIGN KEY (fk_UserId) REFERENCES users(userId) ON DELETE CASCADE,
+  FOREIGN KEY (fk_OrganizationId) REFERENCES organizations(organizationId) ON DELETE CASCADE
 );
 
-CREATE TABLE Groups(
+CREATE TABLE groups(
   groupId INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   `name` VARCHAR(50),
   fk_OrganizationId INT UNSIGNED,
   UNIQUE (fk_OrganizationId, `name`),
-  FOREIGN KEY (fk_OrganizationId) REFERENCES Organizations(organizationId) ON DELETE CASCADE
+  FOREIGN KEY (fk_OrganizationId) REFERENCES organizations(organizationId) ON DELETE CASCADE
 );
 
-CREATE TABLE GroupUsers(
+CREATE TABLE group_users(
   groupUserId INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   fk_UserId INT UNSIGNED,
   fk_GroupId INT UNSIGNED,
   UNIQUE (fk_UserId, fk_GroupId),
-  FOREIGN KEY (fk_UserId) REFERENCES Users(userId) ON DELETE CASCADE,
-  FOREIGN KEY (fk_GroupId) REFERENCES Groups(groupId) ON DELETE CASCADE
+  FOREIGN KEY (fk_UserId) REFERENCES users(userId) ON DELETE CASCADE,
+  FOREIGN KEY (fk_GroupId) REFERENCES groups(groupId) ON DELETE CASCADE
 );
 
-CREATE TABLE GroupChannels(
+CREATE TABLE group_channels(
   groupChannelId INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   `name` VARCHAR(50),
   fk_GroupId INT UNSIGNED,
   UNIQUE (fk_GroupId, `name`),
-  FOREIGN KEY (fk_GroupId) REFERENCES Groups(groupId) ON DELETE CASCADE
+  FOREIGN KEY (fk_GroupId) REFERENCES groups(groupId) ON DELETE CASCADE
 );
 
-CREATE TABLE ChannelMessages(
+CREATE TABLE channel_messages(
   channelMessageId INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   content TEXT,
   fk_UserId INT UNSIGNED NULL,
   fk_GroupChannelId INT UNSIGNED,
-  FOREIGN KEY (fk_UserId) REFERENCES Users(userId) ON DELETE SET NULL,
-  FOREIGN KEY (fk_GroupChannelId) REFERENCES GroupChannels(groupChannelId) ON DELETE CASCADE
+  FOREIGN KEY (fk_UserId) REFERENCES users(userId) ON DELETE SET NULL,
+  FOREIGN KEY (fk_GroupChannelId) REFERENCES group_channels(groupChannelId) ON DELETE CASCADE
 );
 
-CREATE TABLE DirectMessages(
+CREATE TABLE direct_messages(
   directMessageId INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   content TEXT,
   fk_SendingUserId INT UNSIGNED NULL,
   fk_ReceivingUserId INT UNSIGNED NULL,
-  FOREIGN KEY (fk_SendingUserId) REFERENCES Users(userId) ON DELETE SET NULL,
-  FOREIGN KEY (fk_ReceivingUserId) REFERENCES Users(userId) ON DELETE SET NULL
+  FOREIGN KEY (fk_SendingUserId) REFERENCES users(userId) ON DELETE SET NULL,
+  FOREIGN KEY (fk_ReceivingUserId) REFERENCES users(userId) ON DELETE SET NULL
 );
