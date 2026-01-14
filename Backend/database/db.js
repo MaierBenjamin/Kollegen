bc531cd94a283f07fd611bf9ea1ac64b33ccaa6d
Benjamin Maier <benjamin_maier@sluz.ch>
2026-01-14 17:36:46 +0100

    changed DB_PASS to DB_PASSWORD in db.js

--- Backend/database/db.js
+++ Backend/database/db.js
@@ -13,7 +13,7 @@ const MySQLStore = connectMySQL(session)
 const dbOptions = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS, 
+  password: process.env.DB_PASSWORD, 
   database: process.env.DB_NAME,
   connectionLimit: 20,
   enableKeepAlive: true,
